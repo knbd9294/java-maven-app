e5c9fa2429bf249a706fd9e63574f449fb5fa98b
knbd9294 <96204134+knbd9294@users.noreply.github.com>
2026-08-16 09:12:44 -0400

    Implement Docker image build and push in script
    Add Docker image build and push functionality.

--- script.groovy
+++ script.groovy
@@ -5,6 +5,11 @@ def buildJar() {
 
 def buildImage() {
     echo 'building the docker image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t knbd2015/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push knbd2015/demo-app:jma-2.0'
+    }
 } 
 
 def deployApp() {
