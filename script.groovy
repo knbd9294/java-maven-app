a756ddb6c6dd49739bfae1f55b81d6caf3c217a6
knbd9294 <96204134+knbd9294@users.noreply.github.com>
2026-08-16 09:09:30 -0400

    Rename buildApp to buildJar and add buildImage

--- script.groovy
+++ script.groovy
@@ -1,14 +1,14 @@
-def buildApp() {
+def buildJar() {
     echo 'building the application...'
+    sh 'mvn package'
 } 
 
-def testApp() {
-    echo 'testing the application...'
+def buildImage() {
+    echo 'building the docker image...'
 } 
 
 def deployApp() {
     echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
 } 
 
 return this
