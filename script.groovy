bdcaf5eb67774e43763c808274f14d8333344907
knbd9294 <96204134+knbd9294@users.noreply.github.com>
2026-08-17 15:15:48 -0400

    Remove unused functions from script.groovy
    Removed testApp, buildJar, and buildImage functions.

--- script.groovy
+++ script.groovy
@@ -1,22 +1,3 @@
-def testApp() {
-    echo "Testing the application..."
-    echo "Executing pipeline for branch $BRANCH_NAME"
-}
-
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo 'building the docker image...'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t knbd2015/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push knbd2015/demo-app:jma-2.0'
-    }
-} 
-
 def deployApp() {
     echo 'deploying the application...'
 } 
